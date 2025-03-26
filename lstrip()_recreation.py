e4e91608b3fb3670977be02bf26f1956baaf23ed
Fabongita <nashmpiii@gmail.com>
2025-03-26 19:33:02 +0800

    revised the pseudo code and added 2 new variables

--- lstrip()_recreation.py
+++ lstrip()_recreation.py
@@ -1,2 +1,6 @@
-#initialize an input variable for strings
+#initialize an input variable for strings, a flag variable to 
+# track if there are spaces, and variable for the new string 
+string_input = input("input anything: ")
+spaces = True
+new_string = ""
 #use a for loop that loops through the input which checks the whitespace and returns the index if there is no whitespace
