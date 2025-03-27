ad12b5dd0caf1d48f200db810c76b421deb4becd
Fabongita <nashmpiii@gmail.com>
2025-03-28 01:14:28 +0800

    created a variable inside the function that gets only the amount of spaces needed

--- ljust()_recreation_without_using_ljust_itself.py
+++ ljust()_recreation_without_using_ljust_itself.py
@@ -1,7 +1,7 @@
 #create a function for left justify that takes in the string input and spaces
 def left_just(string_input, width):
-    total_width = 
-    return((string_input)+(" "*width)) 
+    total_width = max(len(string_input) - width, 0) 
+    return((string_input)+(" "*total_width)) 
 #create a variable for string input, and width
 string_input = input("input a string: ")
 width = int(input("input how many spaces for the left justification: "))
