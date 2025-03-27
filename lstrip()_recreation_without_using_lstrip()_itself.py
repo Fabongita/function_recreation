f68d73c528d58131e4dd4c41d92f1ae10158d325
Fabongita <nashmpiii@gmail.com>
2025-03-27 21:15:13 +0800

    replaced i for char in for loop

--- lstrip()_recreation_without_using_lstrip()_itself.py
+++ lstrip()_recreation_without_using_lstrip()_itself.py
@@ -4,10 +4,10 @@ string_input = input("input anything: ")
 leading_spaces = True
 new_string = ""
 #use a for loop that loops through the input which checks the whitespace and returns the index if there is no whitespace
-for i in string_input:
-    if leading_spaces and i.isspace():
+for char in string_input:
+    if leading_spaces and char.isspace():
         continue
     else:
         leading_spaces = False
-        new_string += i
+        new_string += char
 print(new_string)
