8ff73ef6dbc409a3657f5062281c03708d267bde
Fabongita <nashmpiii@gmail.com>
2025-03-28 00:12:58 +0800

    added variables

--- endswith()_recreation_without_using_endswith()_itself.py
+++ endswith()_recreation_without_using_endswith()_itself.py
@@ -1,2 +1,5 @@
 #add input variables for the strings and the end string, and a counter variable
+string_input = input("input a string: ")
+end_string = input("input an end string: ")
+counter = 0
 #add a for loop that loops through the input variables in reverse and checks if it is the same with the end string
