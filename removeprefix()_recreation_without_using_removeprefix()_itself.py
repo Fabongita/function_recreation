0bdfb47d2b64e06d8e791b8b5abacc19ea2a87a3
Fabongita <nashmpiii@gmail.com>
2025-03-28 00:55:49 +0800

    changed the prefix from just a string variable , to an input variable

--- removeprefix()_recreation_without_using_removeprefix()_itself.py
+++ removeprefix()_recreation_without_using_removeprefix()_itself.py
@@ -1,6 +1,6 @@
 # initialize a variable for the input, prefix that is to be removed, and a counter
 string_input = input("input anything: ")
-prefix = "hello"
+prefix = input("add a prefix to be removed: ")
 new_string_input = ""
 counter = 0
 #use a for loop that iterates through the input which if the input and the prefix are equal then it will continue
