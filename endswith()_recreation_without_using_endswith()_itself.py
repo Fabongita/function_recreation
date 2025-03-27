998af356d9eb99ec7b3a9f75f5174b5be6de720f
Fabongita <nashmpiii@gmail.com>
2025-03-28 00:09:58 +0800

    added pseudo-code

---
+++ endswith()_recreation_without_using_endswith()_itself.py
@@ -0,0 +1,2 @@
+#add input variables for the strings and the end string, and a counter variable
+#add a for loop that loops through the input variables in reverse and checks if it is the same with the end string
