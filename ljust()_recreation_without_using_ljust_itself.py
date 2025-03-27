291a397b69469ff6576dcc7d29b52fd2697b64a7
Fabongita <nashmpiii@gmail.com>
2025-03-28 00:41:42 +0800

    added a new variable for the amount of space

--- ljust()_recreation_without_using_ljust_itself.py
+++ ljust()_recreation_without_using_ljust_itself.py
@@ -1,3 +1,6 @@
-#create a function for left justify that takes in the string input
+#create a function for left justify that takes in the string input and spaces
+def left_just(string_input):
 
-#create a variable for string input
+#create a variable for string input, and spaces
+string_input = input("input a string: ")
+spaces = int(input("input how many spaces for the left justification: "))
