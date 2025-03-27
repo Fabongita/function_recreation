d96ef92a2934f959b172925bb26f4e8f90ebf8a8
Fabongita <nashmpiii@gmail.com>
2025-03-28 00:29:19 +0800

    fixed the char == end_string logic so that it checks for individual characters and not the whole string

--- endswith()_recreation_without_using_endswith()_itself.py
+++ endswith()_recreation_without_using_endswith()_itself.py
@@ -5,8 +5,9 @@ counter = 0
 
 #add a for loop that loops through the input variables in reverse and checks if it is the same with the end string
 for char in string_input[::-1]:
-    if counter < len(end_string) and char == end_string[::-(counter+1)]:
+    # Compare the current character with the corresponding character from the end of end_string
+    if counter < len(end_string) and char == end_string[-(counter+1)]:
         counter += 1
     else: 
         break
-    
+if 
