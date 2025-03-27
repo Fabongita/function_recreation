1a6d8bb31ece806a0ee2fed582873133ff9d691f
Fabongita <nashmpiii@gmail.com>
2025-03-27 23:20:37 +0800

    fixed the logic and revised the comments

--- isupper()_recreation_without_using_isupper()_itself.py
+++ isupper()_recreation_without_using_isupper()_itself.py
@@ -1,12 +1,10 @@
-# add variables
+# add variables, and a Flag variable
 string_input = input("input a sentence that is either fully capital or not fully capital or both: ")
 checker = True
-# use for loop
+# use for loop that breaks and turns the flag variable to false 
 for char in string_input:
     if char.islower():
         checker = False
-        print(checker)
-        checker = ""
         break
     else:
         continue
