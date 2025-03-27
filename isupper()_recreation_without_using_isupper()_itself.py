73ff62652bddbf16866313cbc608aba2743e25e1
Fabongita <nashmpiii@gmail.com>
2025-03-27 23:06:50 +0800

    added variables, for loop logic, and print statement, but is still incomplete because of the print statement

--- isupper()_recreation_without_using_isupper()_itself.py
+++ isupper()_recreation_without_using_isupper()_itself.py
@@ -1,2 +1,13 @@
 # add variables
+string_input = input("input a sentence that is either fully capital or not fully capital or both: ")
+checker = True
 # use for loop
+for char in string_input:
+    if char.islower():
+        checker = False
+        print(checker)
+        checker = ""
+        break
+    else:
+        continue
+print(checker)
