a729731434e65284112c2aec295dd7af36c782b2
Fabongita <nashmpiii@gmail.com>
2025-03-28 00:21:16 +0800

    finished the for loop logic, but still missing a way to see if all characters match

--- endswith()_recreation_without_using_endswith()_itself.py
+++ endswith()_recreation_without_using_endswith()_itself.py
@@ -2,4 +2,11 @@
 string_input = input("input a string: ")
 end_string = input("input an end string: ")
 counter = 0
+
 #add a for loop that loops through the input variables in reverse and checks if it is the same with the end string
+for char in string_input[::-1]:
+    if counter < len(end_string) and char == end_string[::-(counter+1)]:
+        counter += 1
+    else: 
+        break
+    
