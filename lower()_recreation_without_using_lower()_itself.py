c4120251bc2c666e31ba72074cfa041f323d21f9
Fabongita <nashmpiii@gmail.com>
2025-03-27 22:19:25 +0800

    added variables, for loop logic, and print statement

--- lower()_recreation_without_using_lower()_itself.py
+++ lower()_recreation_without_using_lower()_itself.py
@@ -1,3 +1,11 @@
 # Add input variable, variable for the lowered words
-
+string_input = input("input sentences that has capital letters: ")
+lowered_words = ""
 # for loop logic that iterates through the input variable and checks if it is a small letter or not
+for char in string_input:
+    if char.isupper():
+        swapper = char.swapcase()
+        lowered_words += swapper
+    else:
+        lowered_words += char
+print(lowered_words)
