3e6f28d37e730aabe14cd36b691eea7cd8c9ed7b
Fabongita <nashmpiii@gmail.com>
2025-03-28 01:10:25 +0800

    changed the spaces to width because I misunderstood the characters for just spaces.

--- ljust()_recreation_without_using_ljust_itself.py
+++ ljust()_recreation_without_using_ljust_itself.py
@@ -1,8 +1,9 @@
 #create a function for left justify that takes in the string input and spaces
-def left_just(string_input, spaces):
-    return((string_input)+(" "*spaces)) 
-#create a variable for string input, and spaces
+def left_just(string_input, width):
+    total_width = 
+    return((string_input)+(" "*width)) 
+#create a variable for string input, and width
 string_input = input("input a string: ")
-spaces = int(input("input how many spaces for the left justification: "))
+width = int(input("input how many spaces for the left justification: "))
 #add a print statement 
 print(left_just(string_input, spaces), "Hello, have a good day")
