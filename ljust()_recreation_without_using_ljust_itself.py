cf45e963393fe52ffc8b37d5d638a4165abf6b28
Fabongita <nashmpiii@gmail.com>
2025-03-28 10:30:04 +0800

    changed the order from length of string - width to width - lenght of string

--- ljust()_recreation_without_using_ljust_itself.py
+++ ljust()_recreation_without_using_ljust_itself.py
@@ -1,9 +1,9 @@
 #create a function for left justify that takes in the string input and spaces
 def left_just(string_input, width):
-    total_width = max(len(string_input) - width, 0) 
+    total_width = max(width - len(string_input), 0) 
     return((string_input)+(" "*total_width)) 
 #create a variable for string input, and width
 string_input = input("input a string: ")
 width = int(input("input how many spaces for the left justification: "))
 #add a print statement 
-print(left_just(string_input, spaces), "Hello, have a good day")
+print(left_just(string_input, width), "Hello, have a good day")
