05fb1c09fa92d11690043c652887a2de58b7ede4
Fabongita <nashmpiii@gmail.com>
2025-03-28 13:30:59 +0800

    shortened the if statements by just directly doing string concatenation, added spaces, and changed the second if statement to an elif

--- swapcase()_recreation_without_using_swapcase()_itself.py
+++ swapcase()_recreation_without_using_swapcase()_itself.py
@@ -2,15 +2,15 @@
 string_input = input("input a string with alternating capitalizations: ")
 new_string = ""
 # for loop that checks if an string is a capital or small letter. Then changes to its opposite.
+
 for char in string_input:
     if char.isupper():
-        capital_converter = chr((ord(char)+32))
-        new_string += capital_converter
-    if char.islower():
-        small_converter = chr((ord(char)-32))
-        new_string += small_converter
+        new_string += chr((ord(char)+32))
+    elif char.islower():
+        new_string += chr((ord(char)-32))
     else:
         new_string += char
+
 # add print statement
 print(new_string)
 
