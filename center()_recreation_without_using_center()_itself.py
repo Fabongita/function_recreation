23deef506c07277fa4ad79b69ff177a0f38102c6
Fabongita <nashmpiii@gmail.com>
2025-03-28 11:27:06 +0800

    finished the function logic

--- center()_recreation_without_using_center()_itself.py
+++ center()_recreation_without_using_center()_itself.py
@@ -1,7 +1,8 @@
 # define a function for the main logic of the center method
 def center_alignment(string_input, padding_character, character_number):
-    total_left_padding =
-    total_right_padding =
+    total_padding = max((character_number - len(string_input), 0))
+    total_left_padding = total_padding // 2
+    total_right_padding = total_padding - total_left_padding
     return((total_left_padding*padding_character)+string_input+(total_right_padding*padding_character))
 # initialize variables for the string input, how much padding, and the number of characters
 string_input = input("Input a string: ")
