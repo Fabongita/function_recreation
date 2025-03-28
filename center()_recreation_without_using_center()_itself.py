1d95bc2050c90bb2e0bc9654551d377b216d416c
Fabongita <nashmpiii@gmail.com>
2025-03-28 10:39:12 +0800

    added pseudo-code

---
+++ center()_recreation_without_using_center()_itself.py
@@ -0,0 +1,3 @@
+# define a function for the main logic of the center method
+# initialize variables for the string input, how much padding, and the number of characters
+# Use a print statement to for the output
