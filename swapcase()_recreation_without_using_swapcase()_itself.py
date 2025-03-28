2a9434cb630446d018f8786234bb25d2d3e41431
Fabongita <nashmpiii@gmail.com>
2025-03-28 13:19:04 +0800

    added variables, incomplete for loop, and print statement

--- swapcase()_recreation_without_using_swapcase()_itself.py
+++ swapcase()_recreation_without_using_swapcase()_itself.py
@@ -1,3 +1,13 @@
 # add string input, and the new string
-
+string_input = input("input a string with alternating capitalizations: ")
+new_string = ""
 # for loop that checks if an string is a capital or small letter. Then changes to its opposite.
+for char in string_input:
+    if char.isupper():
+        capital_converter = chr((ord(char)+32))
+        new_string += capital_converter
+    else:
+        small_converter = chr((ord(char)-32))
+        new_string += small_converter
+print(new_string)
+
