dd64939d2108cfe23a54eee963bb6638d7678f2e
Fabongita <nashmpiii@gmail.com>
2025-03-28 14:09:49 +0800

    added a flag variable, and incomplete for loop if logic

--- title()_recreation_without_using_title()_itself.py
+++ title()_recreation_without_using_title()_itself.py
@@ -1,5 +1,11 @@
-# initialize a variable for the string input, and an empty string that will be used as the result
+# initialize a variable for the string input, and an empty string that will be used as the result, added a flag variable
+string_input = "Nash.hello HI, LOWER"
+string_results = ""
+new_word = True
 
 # use a for loop?
+for char in string_input:
+    if char.isalpha() and :
+
 
 #add a print statement that shows the results
