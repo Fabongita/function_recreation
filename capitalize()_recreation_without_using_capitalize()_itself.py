5e6873334664715576d643e13c84e52fb1157120
Fabongita <nashmpiii@gmail.com>
2025-03-28 23:24:59 +0800

    added pseudo-code

---
+++ capitalize()_recreation_without_using_capitalize()_itself.py
@@ -0,0 +1,3 @@
+# add input string input variable, and a new string variable for the result, and a flag variable
+
+# add a for loop that checks each character to see if it is an alpha numeric, and a first letter
