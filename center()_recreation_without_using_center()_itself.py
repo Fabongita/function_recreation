c457cfc2268df61d5e1092427bc62604d8634cdd
Fabongita <nashmpiii@gmail.com>
2025-03-28 11:05:48 +0800

    added incomplete function, variables, and print statement

--- center()_recreation_without_using_center()_itself.py
+++ center()_recreation_without_using_center()_itself.py
@@ -1,3 +1,11 @@
 # define a function for the main logic of the center method
+def center_alignment(string_input, padding_character, character_number):
+    pass
 # initialize variables for the string input, how much padding, and the number of characters
+string_input = input("Input a string: ")
+
+padding_character = input("Input the character for the padding, (input only one character, and blank for 1 space): ")
+character_number = int(input("input how many characters to use: "))
+
 # Use a print statement to for the output
+print(center_alignment(string_input, padding_character, character_number))
