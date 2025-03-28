4903923cb6d038542fa31a806b64349ba81366ba
Fabongita <nashmpiii@gmail.com>
2025-03-28 14:41:44 +0800

    Completed the for loop logic, revised the comments, and added print statement

--- title()_recreation_without_using_title()_itself.py
+++ title()_recreation_without_using_title()_itself.py
@@ -1,9 +1,9 @@
 # initialize a variable for the string input, and an empty string that will be used as the result, added a flag variable
-string_input = "Nash.hello HI, LOWER"
+string_input = "HELLO WORLD!"
 string_results = ""
 new_word = True
 
-# use a for loop?
+# use a for to check each characters and see if it it's a first letter, non alpha numeric, and a succeeding letter that is capitalized
 for char in string_input:
     if char.isalpha() and new_word:
         string_results += char.upper()
@@ -13,10 +13,6 @@ for char in string_input:
     else:
         string_results += char
         new_word = True
-
-
-        
-
 
 #add a print statement that shows the results
 print(string_results)
