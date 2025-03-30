22259d0defbeeebc9feba051819b26464694cd33
Fabongita <nashmpiii@gmail.com>
2025-03-30 22:52:54 +0800

    added variables, for loop, and print statement. But it's not outputting what I need

--- rstrip()_recreation_without_using_rstrip()_itself.py
+++ rstrip()_recreation_without_using_rstrip()_itself.py
@@ -1,5 +1,13 @@
 # Add a string input, flag, and a new string variable
-
+string_input = input("input a sentence with leading or trailing spaces: ")
+trailing_spaces = True
+new_string = ""
 # add a for loop that checks if there are spaces on the right side and removes it
-
+for char in string_input[::-1]:
+    if char.isspace() and trailing_spaces:
+        continue
+    else:
+        trailing_spaces = False
+        new_string += char
 # add a print statement for the result
+print(new_string)
