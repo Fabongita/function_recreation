e070017c1c7f36206d8b7fcb91659a8cb51a2291
Fabongita <nashmpiii@gmail.com>
2025-03-31 12:43:46 +0800

    added variables, for loop, and print statement

--- upper()_recreation_without_using_upper()_itself.py
+++ upper()_recreation_without_using_upper()_itself.py
@@ -1,5 +1,13 @@
 # add string input variables, and a new string input variable
+string_input = input("input a string: ")
+new_string = ""
 
 # add a for loop that checks if each character is a lowercase which converts it into uppercase
+for char in string_input:
+    if char.islower():
+        new_string+=chr(ord(char)-32)
+    else:
+        new_string += char
 
 #add print statement for the results
+print(new_string)
