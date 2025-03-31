8fb5420fa1aa970d9a614632f6b451b28098c4d8
Fabongita <nashmpiii@gmail.com>
2025-03-31 14:24:18 +0800

    added function, variables, if else, and print statement

--- rjust()_recreation_without_using_rjust()_itself.py
+++ rjust()_recreation_without_using_rjust()_itself.py
@@ -1,9 +1,17 @@
 # define function that returns the value of the string input with the appropriate spaces
-def right_just():
-    pass
-
+def right_just(string_input, length, character_type):
+    total_width = max(0, (length - len(string_input)))
+    return((total_width*character_type)+string_input)
 # add input variables, the type of character, and the length of the characters
+string_input = input("input a string: ")
+length = int(input("input the length for the characters: "))
+character_type = input("input the type of character to use, (blank for space and only one digit)")
 
 # if and else that checks the type of character
+if not character_type:
+    character_type = " "
+elif len(character_type) > 1:
+    character_type = character_type[0]
 
 # print statement
+print(right_just(string_input, length, character_type), "HAHAAHAH")
