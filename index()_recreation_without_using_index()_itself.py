1e44bea125e806b7dbc91fc1d76e0807c6ad7a35
Fabongita <nashmpiii@gmail.com>
2025-03-31 17:31:46 +0800

    added items in the string list

--- index()_recreation_without_using_index()_itself.py
+++ index()_recreation_without_using_index()_itself.py
@@ -1,5 +1,5 @@
 # add variables for: list, string counter,  and the string itself that is to be counted
-string_list =  ["apple", "spam"]
+string_list =  ["apple", "spam", "hippo", "hippo"]
 string_to_count = "hippo"
 # use a for loop that iterates through the list which counts the specified string
 for index, item in enumerate(string_list):
