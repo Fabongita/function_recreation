c8ddadc497c8d06df885898f41a8b09786f2ad2f
Fabongita <nashmpiii@gmail.com>
2025-03-31 14:28:10 +0800

    added character feature

--- ljust()_recreation_without_using_ljust_itself.py
+++ ljust()_recreation_without_using_ljust_itself.py
@@ -1,11 +1,16 @@
 #create a function for left justify that takes in the string input and spaces
-def left_just(string_input, width):
+def left_just(string_input, width, character):
     total_width = max(width - len(string_input), 0) 
-    return((string_input)+(" "*total_width)) 
+    return((string_input)+(character*total_width)) 
 
 #create a variable for string input, and width
 string_input = input("input a string: ")
 width = int(input("input how many spaces for the left justification: "))
+character = input("input the character to be used, use only 1 digit and a blank is a space: ")
+if not character:
+    character = " "
+elif len(character) > 1:
+    character = character[0]
 
 #add a print statement 
-print(left_just(string_input, width), "Hello, have a good day")
+print(left_just(string_input, width, character), "Hello, have a good day")
