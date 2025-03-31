3e40d3093dc88a5bc59bf2807aba8e521f8af6bc
Fabongita <nashmpiii@gmail.com>
2025-03-31 14:54:20 +0800

    added function, input variables, and print statement

--- zfill()_recreation_without_using_zfill()_itself.py
+++ zfill()_recreation_without_using_zfill()_itself.py
@@ -1,5 +1,11 @@
-# define a function that returns the value of the string with the appropriate Z
-
+# define a function that returns the value of the string with the appropriate Zeros
+def zero_fill(input_string, length):
+    total_width = max(0,(length - len(input_string)))
+    return (("0"*total_width)+input_string)
+    
 # input variables: string, and length
+input_string = input("input string: ")
+length = int(input("input the length for the Zero: "))
 
 # print statement
+print(zero_fill(input_string, length))
