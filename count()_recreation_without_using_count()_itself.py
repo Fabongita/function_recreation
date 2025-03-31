5debfc9cc105511a7ab6d759ed14ce2d07b25d22
Fabongita <nashmpiii@gmail.com>
2025-03-31 16:34:40 +0800

    added pseudo-code

---
+++ count()_recreation_without_using_count()_itself.py
@@ -0,0 +1,5 @@
+# add variables for: list, string counter,  and the string itself that is to be counted
+
+# use a for loop that iterates through the list which counts the specified string
+
+# add a print statement that show the results 
