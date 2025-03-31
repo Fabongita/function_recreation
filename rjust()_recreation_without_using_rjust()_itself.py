b4a964bfc52fb560caf766461474c8fd82c78dc6
Fabongita <nashmpiii@gmail.com>
2025-03-31 13:59:50 +0800

    added pseudo-code

---
+++ rjust()_recreation_without_using_rjust()_itself.py
@@ -0,0 +1,9 @@
+# define function that returns the value of the string input with the appropriate spaces
+def right_just():
+    pass
+
+# add input variables, the type of character, and the length of the characters
+
+# if and else that checks the type of character
+
+# print statement
