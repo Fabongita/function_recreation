2cb9427f764b7d113d33b4305ab213a8e96fea3f
Fabongita <nashmpiii@gmail.com>
2025-03-31 19:33:12 +0800

    added items in the string list, and added file for program 10 batch 2

--- index()_recreation_without_using_index()_itself.py
+++ index()_recreation_without_using_index()_itself.py
@@ -1,5 +1,5 @@
 # add variables for: list, string counter,  and the string itself that is to be counted
-string_list =  ["apple", "spam", "hippo", "hippo"]
+string_list =  ["apple", "hippo", "spam", "hippo" ]
 string_to_count = "hippo"
 # use a for loop that iterates through the list which counts the specified string
 for index, item in enumerate(string_list):
@@ -7,6 +7,6 @@ for index, item in enumerate(string_list):
         found_index = index
         break
 else:
-    found_index = "ValueError" 
+    raise ValueError(f"{string_to_count} not found in the list")
 # add a print statement that show the results 
 print(found_index)
