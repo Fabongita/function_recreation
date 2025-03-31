fd5051a683483b4a62504e15f743af2fba220e1d
Fabongita <nashmpiii@gmail.com>
2025-03-31 12:40:47 +0800

    added new file for program 3 batch 2, and also added pseudo-code

---
+++ upper()_recreation_without_using_upper()_itself.py
@@ -0,0 +1,5 @@
+# add string input variables, and a new string input variable
+
+# add a for loop that checks if each character is a lowercase which converts it into uppercase
+
+#add print statement for the results
