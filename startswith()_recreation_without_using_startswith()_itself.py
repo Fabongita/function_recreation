f01f480068a0babb79f2025c35ce9d93946a2991
Fabongita <nashmpiii@gmail.com>
2025-03-31 13:50:42 +0800

    added variables, and if else statements

--- startswith()_recreation_without_using_startswith()_itself.py
+++ startswith()_recreation_without_using_startswith()_itself.py
@@ -1,3 +1,8 @@
 # add input string, and starts with variable
-
+string_input = input("input a string: ")
+starts_with = input("input a start string: ")
 # add if and else that checks if the input starts with the starts variable
+if string_input[:len(starts_with)] == starts_with:
+    print("True")
+else:
+    print("False")
