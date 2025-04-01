f485af5abf42f26bb06ffc18cd4e97e3c91c9de0
Fabongita <nashmpiii@gmail.com>
2025-04-01 16:10:44 +0800

    added incomplete pseudo-code

---
+++ rindex()_recreation_without_using_rindex()_itself.py
@@ -0,0 +1,8 @@
+# add string and string to find for testing (change to input statement if finished)
+new_string = "Mi casa, su casa."
+index_string = "casa."
+
+#reverse an index using index ()
+
+
+
