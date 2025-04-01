f6412481057ddc437dffed32d12bd9e784eef22b
Fabongita <nashmpiii@gmail.com>
2025-04-01 16:34:28 +0800

    added variables for reversing the index without using rindex(). and added print statement

--- rindex()_recreation_without_using_rindex()_itself.py
+++ rindex()_recreation_without_using_rindex()_itself.py
@@ -1,8 +1,12 @@
 # add string and string to find for testing (change to input statement if finished)
 new_string = "Mi casa, su casa."
-index_string = "casa."
+index_string = "k"
 
-#reverse an index using index ()
+#reverse an index using index () to find the last occurence
+reversed_index = new_string[::-1].index(index_string[::-1])
+reverse_result = len(new_string) - reversed_index - len(index_string)
 
+# print the result 
+print(reverse_result)
 
 
