7a95def61e566149e5f4373352d1b3e3465e50c5
Fabongita <nashmpiii@gmail.com>
2025-04-01 16:37:43 +0800

    changed the string variables to a string input variable

--- rindex()_recreation_without_using_rindex()_itself.py
+++ rindex()_recreation_without_using_rindex()_itself.py
@@ -1,6 +1,6 @@
 # add string and string to find for testing (change to input statement if finished)
-new_string = "Mi casa, su casa."
-index_string = "k"
+new_string = input("input a string: ")
+index_string = input("input the number of index to find on the string: ")
 
 #reverse an index using index () to find the last occurence
 reversed_index = new_string[::-1].index(index_string[::-1])
